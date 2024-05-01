2b65a24cf023b1506d3118fde4b9f3d68308a27e
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2024-05-01 12:46:52 +0700

    Feat: Melengkapi Data Halaman Detail Karakter

--- app/components/offcanvas/index.js
+++ app/components/offcanvas/index.js
@@ -2,24 +2,39 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
-export function OffCanvas({ ...props }) {
+export function OffCanvas({ data }) {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const backgroundColor = data?.status == "Alive" ? '#C2F5E9' : data?.status == "Dead" ? '#FFDCE5' : '#D0F0FD';
+    const color = data?.status == "Alive" ? '#06A09B' : data?.status == "Dead" ? '#BA1E45' : '#01A9DB';
 
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
                 Detail
             </Button>
-            <Offcanvas show={show} onHide={handleClose} placement='end' {...props}>
+            <Offcanvas show={show} onHide={handleClose} placement='end' >
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                    <Offcanvas.Title>
+                        Detail {data?.name}
+                    </Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                    Some text as placeholder. In real life you can have the elements you
-                    have chosen. Like, text, images, lists, etc.
+                    <div className='d-flex align-items-center justify-content-between'>
+                        <div className='d-flex align-items-center'>
+                            <img src={data?.image} alt={data?.name} loading="eager" className='rounded-circle' width={40} height={40} />
+                            <div className='ms-3'>
+                                <div className='fw-semibold' style={{ fontSize: 14, color: '#333333' }}>{data?.name}</div>
+                                <div className='mt-1' style={{ fontSize: 12, color: '#757575' }}>{data?.gender}</div>
+                            </div>
+                        </div>
+                        <div style={{ width: 109, height: 24, backgroundColor: backgroundColor, border: `1px solid ${color}`, display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: 12, borderRadius: 25, color: color }}>
+                            {data?.status}
+                        </div>
+                    </div>
                 </Offcanvas.Body>
             </Offcanvas>
         </>
--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { CardComponents } from './components/cards';
 import { fetchData } from './library/api';
 import { OffCanvas } from './components/offcanvas';
+import styles from './page.module.css'
 
 export default function Page() {
   const [listCharacters, setListCharacters] = useState([]);
@@ -15,14 +16,14 @@ export default function Page() {
 
   return (
     <div className='container'>
-      <div className='row py-4 grid gap-3 column-gap-3'>
+      <div className='row p-3 grid gap-3 column-gap-3 justify-content-center'>
         {Array.isArray(listCharacters?.results) && listCharacters?.results?.length == 0 ? null :
           listCharacters?.results?.map((items, index) => {
             return (
-              <>
-                <CardComponents key={index} sourceImg={items.image} name={items.name} species={items.species} />
-                {/* <OffCanvas /> */}
-              </>
+              <div className={`bg-white border py-3 shadow-sm rounded-3 d-flex flex-column justify-content-center align-items-center ${styles.custom_width}`}>
+                <CardComponents key={index} sourceImg={items.image} name={items.name} gender={items.gender} />
+                <OffCanvas data={items} />
+              </div>
             )
           })
         }
